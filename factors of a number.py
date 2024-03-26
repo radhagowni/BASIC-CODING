870748afdc1a6955ce488f52ae0753c8e2b66b7d
radhagowni <136911875+radhagowni@users.noreply.github.com>
2024-03-26 07:14:10 +0530

    Add files via upload

---
+++ factors of a number.py
@@ -0,0 +1,9 @@
+def find_divisors(n):
+    if n<=0:
+        return
+    else:
+        for i in range(1,n+1):
+            if n%i==0:
+                print(i,end=' ')
+print('factors : ')
+find_divisors(100)
