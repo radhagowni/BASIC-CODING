1657e5d7ebffdc4a18cd54768f51f267a3ce3be9
radhagowni <136911875+radhagowni@users.noreply.github.com>
2024-07-23 19:45:47 +0530

    Add files via upload

---
+++ finding percentage of a given student among several students.py
@@ -0,0 +1,11 @@
+n=int(input())
+student_marks={}
+for i in range(n):
+    name,*l=input().split()
+    scores=list(map(float,l))
+    student_marks[name]=scores
+query_name=input()
+for j in student_marks:
+    if j==query_name:
+        average=sum(student_marks[j])/3
+print("{:.2f}".format(average))
