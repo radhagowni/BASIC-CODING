db3c1b80697fa71087f5ccd7fb6ae03c0dd913ac
radhagowni <136911875+radhagowni@users.noreply.github.com>
2024-07-30 17:47:18 +0530

    Add files via upload

---
+++ abundant number.py
@@ -0,0 +1,26 @@
+#abundant number:the sum of the factors is greater than the number
+#number from user
+n=int(input())
+#intialising sum as 1 because 1 is divisor of every number
+sum=1
+for i in range(2,n):
+    if n%2==0:
+        sum=sum+i
+if sum>n:
+    print("abundant number")
+else:
+    print("not an abundant number")
+#alternate method
+i=2
+import math
+while i<=math.sqrt(n):
+    if n%2==0:
+        if n//i==i:
+            sum=sum+i
+        else:
+            sum=sum+i+n/i
+            i=i+1
+if sum>n:
+    print("abundant number")
+else:
+    print("not an abundant number")
