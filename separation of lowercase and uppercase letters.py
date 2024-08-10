1695cb91a47d791a7b2b4f5b14fe1f7659ecd91a
radhagowni <136911875+radhagowni@users.noreply.github.com>
2024-08-10 19:42:00 +0530

    Add files via upload

---
+++ separation of lowercase and uppercase letters.py
@@ -0,0 +1,23 @@
+#taking input from string
+string=input()
+#initialising empty variables to store upper and lower case letters separately
+s=""
+r=""
+#traversing through string
+for i in range(len(string)):
+    if string[i].islower():
+        s=s+string[i]
+    else:
+        r=r+string[i]
+print("lower case letters:",s)
+print("uppercase letters:",r)
+#alternative way
+l=""
+m=""
+for i in range(len(string)):
+    if string[i]>='a'  and string[i]<='z':
+        l=l+string[i]
+    else:
+        m=m+string[i]
+print(s)
+print(r)
