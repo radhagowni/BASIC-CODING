f2bc2d1edaebadcf2fb4a1e293aa81abf219244c
radhagowni <136911875+radhagowni@users.noreply.github.com>
2024-08-15 16:09:41 +0530

    Add files via upload

---
+++ calculating born day.py
@@ -0,0 +1,18 @@
+month=[0,3,3,6,1,4,6,2,5,0,3,5]
+day=["sun","mon","tue","wed","thu","fri","sat"]
+y=0
+date=int(input("enter date:"))
+month1=int(input("enter month:"))
+year=int(input("enter year:"))
+if (year>1600 and year<=1700):
+    y=6
+elif (year>1700 and year<=1800):
+    y=4
+elif (year>1800 and year<=1900):
+    y=2
+elif (year>1900  and year<=2000):
+    y=0
+elif (year>2000 and year<=2100):
+    y=6
+bday=day[(date+y+month[month1-1]+((year%100)//4)+(year%100))%7]
+print(bday)   
