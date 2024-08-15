d478100c664c0ed55985e439bbe6b71f0f24f684
radhagowni <136911875+radhagowni@users.noreply.github.com>
2024-08-15 16:10:13 +0530

    Add files via upload

---
+++ simple interest and emi.py
@@ -0,0 +1,15 @@
+def simple_interest(p,t,r):
+    return (principle*time*rate)/100
+def EMI(p,t,r):
+    i=(p*t*r)/100
+    emi=(p+i)/(12*t)
+    return emi
+principle=int(input("enter the principle amount:"))
+time=int(input("enter time: "))
+rate=int(input("enter rate of interest:"))
+interest=simple_interest(principle,time,rate)
+emi=EMI(principle,time,rate)
+print("simple interest:",interest)
+print("EMI per month:",emi)
+print("the amount to be paid from principle amount as emi:",(principle/(12*time)))
+print("the amount to be amount from interest as emi:",(interest)/(12*time))
