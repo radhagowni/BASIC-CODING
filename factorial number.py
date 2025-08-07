fb928e964f903f6254b9a1f382be2f322b2377d8
Gowni Radha <136911875+radhagowni@users.noreply.github.com>
2025-08-07 22:24:51 +0530

    Update factorial number.py

--- factorial number.py
+++ factorial number.py
@@ -1,9 +1,7 @@
 n=int(input('enter number:'))
 fact=1
-if n<2:
-    print('not possible')
-elif n==0:
-    print('1')
+if n==0 or n==1:
+    print(1)
 for i in range(1,n+1):
     fact=fact*i
 print('factorial of a number:',fact)
