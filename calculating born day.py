66c470ff6282bcd952b7e07f5f8f0022fe7bcbb3
Gowni Radha <136911875+radhagowni@users.noreply.github.com>
2025-08-17 20:29:09 +0530

    Update calculating born day.py

--- calculating born day.py
+++ calculating born day.py
@@ -1,9 +1,9 @@
 month=[0,3,3,6,1,4,6,2,5,0,3,5]
 day=["sun","mon","tue","wed","thu","fri","sat"]
 y=0
-date=int(input("enter date:"))
-month1=int(input("enter month:"))
-year=int(input("enter year:"))
+date=int(input("Enter date:"))
+month1=int(input("Enter month:"))
+year=int(input("Enter year:"))
 if (year>1600 and year<=1700):
     y=6
 elif (year>1700 and year<=1800):
