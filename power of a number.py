9d4c0c47719c7f15c839c10af9ce0c41be13c9c3
Gowni Radha <136911875+radhagowni@users.noreply.github.com>
2025-11-20 21:53:22 +0530

    Update power of a number.py

--- power of a number.py
+++ power of a number.py
@@ -1,5 +1,5 @@
 #code for power of a number
-num=int(input())
+num=int(input("Enter base value: " ))
 power=int(input())
 print('Power of',num,'is',pow(num,power))
 print('alternate way:',num**power)
