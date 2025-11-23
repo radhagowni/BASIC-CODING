3d6f0157e27765a9fa80a85bd28273de27d9d07c
Gowni Radha <136911875+radhagowni@users.noreply.github.com>
2025-11-23 15:22:35 +0530

    Update count of substring in a string.py

--- count of substring in a string.py
+++ count of substring in a string.py
@@ -1,5 +1,5 @@
 # To calculate the number of times the substring has occured in the given string
-string=input("enter string:")
+string=input("Enter string:")
 substring=input()
 count=0
 for i in range(0,len(string)):
